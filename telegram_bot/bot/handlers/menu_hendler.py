040db468c434f5b92dd5c7e9619d5c0106d1990c
alexlukyanets <alexlukyanets18@gmail.com>
2023-12-05 20:48:33 +0200

    add markdown parsing

--- telegram_bot/bot/handlers/menu_hendler.py
+++ telegram_bot/bot/handlers/menu_hendler.py
@@ -1,3 +1,4 @@
+from aiogram.enums import ParseMode
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 
@@ -10,4 +11,5 @@ async def menu_handler(message: Message, state: FSMContext) -> None:
     current_state = await state.get_state()
     menu_items, keyboard_type, message_reply = await CORE_USE_CASE.get_menu(message.text, current_state)
     await message.answer(message_reply,
+                         parse_mode=ParseMode.MARKDOWN,
                          reply_markup=build_flex_keyboard(menu_items, keyboard_type))
